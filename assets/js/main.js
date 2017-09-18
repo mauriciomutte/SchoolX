0633b017874c17d2c589312cd3bfe6bca54ccf21
Maurício Mutte <hi@mauriciomutte.com>
2017-09-18 17:58:51 -0300

    Add schedule

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
+// header var
 const menuBtn = document.querySelector('.menu__btn')
 const menuNav = document.querySelector('.menu__nav')
 const settingBtn = document.querySelector('.setting__btn')
 const settingNav = document.querySelector('.setting__nav')
+
+//
+// HEADER
+//
 
 menuBtn.addEventListener('click', function() {
   event.stopPropagation();
@@ -21,3 +26,49 @@ window.onclick = function(event) {
     settingNav.classList.remove('setting__nav--show')
   }
 }
+
+//
+// SCHEDULE
+// 
+
+function scheduleTable() {
+  const week = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex']
+  const firstPeriod = ['Art', 'Bio', 'Fís', 'Port', 'Filo']
+  const secondPeriod = ['Art', 'Bio', 'Fís', 'Port', 'Fís']
+  const thirdPeriod = ['Red', 'Mat', 'Socio', 'Hist', 'Mat']
+  const fourthPeriod = ['Qui', 'Mat', 'Esp', 'Hist', 'Mat']
+  const fifthPeriod = ['Ed.F', 'Qui', 'Lit', 'Geo', 'Ing']
+  const sixthPeriod = ['Ed.F', 'Qui', 'Bio', 'Geo', 'Ing']
+  
+  const tbody = document.querySelector('.schedule__tbody')
+
+  const tr1 = document.createElement('tr')
+  tbody.appendChild(tr1)
+
+  for (let i = 0; i < week.length; i++) {
+    const th = document.createElement('th')
+    th.classList.add('table__th')
+    th.innerHTML = week[i]
+    tr1.appendChild(th)
+  }
+
+  function periodTr(period) {
+    const tr2 = document.createElement('tr')
+    tbody.appendChild(tr2)
+
+    for (let i = 0; i < firstPeriod.length; i++) {
+      const td = document.createElement('td')
+      td.classList.add('table__td')
+      td.innerHTML = period[i]
+      tr2.appendChild(td)
+    }
+  }
+  periodTr(firstPeriod)
+  periodTr(secondPeriod)
+  periodTr(thirdPeriod)
+  periodTr(fourthPeriod)
+  periodTr(fifthPeriod)
+  periodTr(sixthPeriod)
+}
+
+scheduleTable()
