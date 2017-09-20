7222c66bbd94b60128d67e3af7e59a08579c8521
Maurício Mutte <hi@mauriciomutte.com>
2017-09-20 20:14:25 -0300

    Add grades-matters in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,53 @@ function scheduleTable() {
 }
 
 scheduleTable()
+
+//
+// GRADES
+// 
+
+function gradesTable() {
+  const gradesTh = ['Matéria', '1 Tri', '2 Tri', '3 Tri']
+  const tbody = document.querySelector('.grades__tbody')
+
+  const tr1 = document.createElement('tr')
+  tbody.appendChild(tr1)
+
+  for (let i = 0; i < gradesTh.length; i++) {
+    const th = document.createElement('th')
+    th.classList.add('grades__th')
+    th.innerHTML = gradesTh[i]
+    tr1.appendChild(th)
+  }
+
+  function gradesMatter(matter, firstTrimester, secondTrimester, thirdTrimester){
+    const gradesTd = [matter, firstTrimester, secondTrimester, thirdTrimester]
+
+    const tr2 = document.createElement('tr')
+    tbody.appendChild(tr2)
+
+    for (let i = 0; i < gradesTd.length; i++) {
+      const tdMatter = document.createElement('td')
+      tdMatter.classList.add('grades__td')
+      tdMatter.innerHTML = gradesTd[i]
+      tr2.appendChild(tdMatter)    
+    }
+  }
+  gradesMatter('Física', '10,0', '10,0', '10,0')
+  gradesMatter('Matemática', '10,0', '10,0', '10,0')
+  gradesMatter('Química', '10,0', '10,0', '10,0')
+  gradesMatter('Biologia', '10,0', '10,0', '10,0')
+  gradesMatter('Filosofia', '10,0', '10,0', '10,0')
+  gradesMatter('Sociologia', '10,0', '10,0', '10,0')
+  gradesMatter('Geografia', '10,0', '10,0', '10,0')
+  gradesMatter('História', '10,0', '10,0', '10,0')
+  gradesMatter('Português', '10,0', '10,0', '10,0')
+  gradesMatter('Literatura', '10,0', '10,0', '10,0')
+  gradesMatter('Redação', '10,0', '10,0', '10,0')
+  gradesMatter('Inglês', '10,0', '10,0', '10,0')
+  gradesMatter('Espanhol', '10,0', '10,0', '10,0')
+  gradesMatter('Arte', '10,0', '10,0', '10,0')
+  gradesMatter('Ed. Fis', '10,0', '10,0', '10,0')
+}
+
+gradesTable()
