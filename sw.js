32348a9309dc903a9f0d383e795179694ee6778a
Mauricio Mutte <litiobr@gmail.com>
2018-03-17 18:46:41 -0300

    update sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v2';
-const RUNTIME = 'runtime';
+const PRECACHE = 'precache-v3';
+const RUNTIME = 'runtime-v1';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
