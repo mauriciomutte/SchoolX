12fb4a44f424a264c32808f969a4c50b680e3345
Mauricio Mutte <litiobr@gmail.com>
2018-06-22 22:42:39 -0300

    Add 'ConteudoX' js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,3 +275,45 @@ createTestTable(matematica, '22/12', 'recuperação')
 createTestTable(quimica, '22/12', 'recuperação')
 createTestTable(redacao, '22/12', 'recuperação')
 createTestTable(sociologia, '22/12', 'recuperação')
+
+//ConteudoX
+function matterList(trimestre, materia) {
+  const list = document.querySelector('.' + materia +'-lista')
+
+  const divList = document.createElement('div')
+  divList.classList.add('lista__trimestre')
+  list.appendChild(divList)
+
+  const h2List = document.createElement('h2')
+  h2List.classList.add('lista__trimestre--title')
+  h2List.innerHTML = trimestre.title
+  divList.appendChild(h2List)
+
+  const trimestreLista = document.createElement('ul')
+  trimestreLista.classList.add('lista__trimestre__conteudo--ul')
+  divList.appendChild(trimestreLista)
+
+  for (let i = 0; i < trimestre.content.length; i++) {
+    const conteudo = document.createElement('li')
+    conteudo.classList.add('lista__trimestre__conteudo--li')
+    conteudo.innerHTML = trimestre.content[i]
+    trimestreLista.appendChild(conteudo)
+  }
+}
+
+function callJSON(m, materia, ano) {
+  const require = new XMLHttpRequest()
+  require.open("GET", '/ConteudoX/' + ano + '.json', true)
+  require.onload = function() {
+    const matter = JSON.parse(this.responseText)
+    
+    const test = [matter.fisica, matter.matematica, matter.quimica, matter.biologia, matter.filosofia, matter.sociologia,
+    matter.geografia, matter.historia, matter.portugues, matter.literatura, matter.redacao, matter.ingles, matter.espanhol,
+    matter.arte, matter.edf]
+
+    matterList(test[m].firstTri, materia)
+    matterList(test[m].secondTri, materia)
+    matterList(test[m].thirdTri, materia)
+  }
+  require.send()
+}
