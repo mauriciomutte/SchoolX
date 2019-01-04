a644eb46d99aaf25c4bf303b38f0ccdb9f9d3e3e
Maurício Mutte <litiobr@gmail.com>
2019-01-03 22:10:19 -0200

    Add dinamic insert books in 'leituras'

---
+++ assets/js/leituras.js
@@ -0,0 +1,35 @@
+const require = new XMLHttpRequest();
+require.open('GET', '../../leituras.json', true);
+require.onload = function() {
+  const livrosList = JSON.parse(this.responseText);
+
+  function createBook(livroObj) {
+    const main = document.querySelector('.leituras');
+
+    const book = document.createElement('div');
+    book.classList.add('leituras__book');
+    main.appendChild(book);
+  
+    const bookImg = document.createElement('img');
+    bookImg.classList.add('leituras__book__capa');
+    bookImg.src = livroObj.img;
+    book.appendChild(bookImg);
+  
+    const bookUl = document.createElement('ul');
+    bookUl.classList.add('leituras__book__ul');
+    book.appendChild(bookUl);
+  
+    livroObj.dados.forEach(function(item) {
+      const bookLi = document.createElement('li');
+      bookLi.classList.add('leituras__book__li');
+      bookLi.innerHTML = item;
+      bookUl.appendChild(bookLi);
+    });
+  };
+
+  livrosList.forEach(function(item) {
+    createBook(item);
+  });
+  
+};
+require.send();
